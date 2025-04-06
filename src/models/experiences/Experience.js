f6dd322253aa826d40281de13b13e34d699a97f2
operator-point-zero <projectnoon987@gmail.com>
2025-04-06 06:36:55 -0700

    so many fixes
    repetition of experiences and titles,
    repetition of pics,
    unrelatable pics,
    repetiton of location in different experiences

--- src/models/experiences/Experience.js
+++ src/models/experiences/Experience.js
@@ -26,37 +26,3 @@ const Experience = mongoose.model("Experience", experienceSchema);
 
 export default Experience;
 
-// import mongoose from "mongoose";
-
-// const locationSchema = new mongoose.Schema({
-//   lat: { type: Number, required: true },
-//   lon: { type: Number, required: true },
-//   locationName: { type: String, required: true },
-//   placeId: { type: String, required: false },
-//   types: { type: [String], required: false },
-//   rating: { type: String, required: false },
-//   vicinity: { type: String, required: false },
-//   photos: { type: [String], required: false }, // Array of photo URLs
-//   narration: { type: String, required: false },
-// });
-
-// const experienceSchema = new mongoose.Schema(
-//   {
-//     title: { type: String, required: true },
-//     description: { type: String, required: true },
-//     locations: { type: [locationSchema], required: true },
-//     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     is_seed: { type: Boolean, default: false }, // Add is_seed field.
-//     times_shown: { type: Number, default: 0 }, // Add times_shown field.
-//     source_experience_id: { type: mongoose.Schema.Types.ObjectId, ref: "Experience", required: false }, // Add source_experience_id field.
-//     location_center: {
-//       lat: { type: Number, required: false },
-//       lon: { type: Number, required: false },
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const Experience = mongoose.model("Experience", experienceSchema);
-
-// export default Experience;
